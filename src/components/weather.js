c2004b39f6ef8e1250e8142aabc01326aefaba1c
HitmanJoshua <53341657+HitmanJoshua@users.noreply.github.com>
2019-07-30 05:42:05 -0700

    Add files via upload

---
+++ src/components/weather.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+export default class Weather extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { weather: null };
+    }
+
+    componentDidMount() //{
+    {
+        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=b25902b9a7c980f9bb05ebc8f7c93918`)
+            .then(response => {
+                // console.log(response);
+                const data = JSON.parse(JSON.stringify(response.data));
+                // console.log(data);
+                this.setState({ weather: data });
+                console.log(this.state.weather);
+            })
+    }
+
+
+    render() {
+            // let x=this 
+        return (
+            // // <div>
+            //     {/* <ul>
+            //         {this.state.weather.map(weather => {weather.id})} }</ul> */}
+            //     {/* {this.state.weather.id} */}
+            // // </div>
+            <div>
+            {/* {this.state.weather.cood} */}
+            </div>
+        );
+    }
+}
